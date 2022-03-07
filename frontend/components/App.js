d4b45b234eb69ce559744af157a4466ee54a3324
Joseph Fantuzzi <dev.josephfantuzzi@gmail.com>
2022-03-07 15:39:06 -0500

    slices of state in place

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,11 +1,27 @@
-import React from 'react'
+import React from "react";
+
+let index = 0;
+const getIndex = () => {
+  return ++index;
+};
+
+const toDos = [
+  {
+    name: "",
+    id: getIndex(),
+    completed: false,
+  },
+];
+
+const initialState = {
+  toDos,
+  toDoInput: "",
+};
 
 export default class App extends React.Component {
+  state = initialState;
+
   render() {
-    return (
-      <div>
-        Todo App
-      </div>
-    )
+    return <div>Todo App</div>;
   }
 }
